b30c7ff9fdb44f92965e1805e8045c0e0793d6f4
Muhammed Yusuf Mermer <myusufmermer@gmail.com>
2024-12-04 22:55:11 +0300

    minor update on dataset folder (removed)

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,6 +2,6 @@ class Config:
     DB_HOST = "127.0.0.1"
     DB_PORT = 3306
     DB_USER = "root"
-    DB_PASSWORD = "mym123"
+    DB_PASSWORD = "iamnotsharingmypaswordwithyou"
     DB_NAME = "basketball"
     DEBUG = True
