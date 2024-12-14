3e5f85cc8efcca40a4c5e26e81af6c6d28824de2
Muhammed Yusuf Mermer <myusufmermer@gmail.com>
2024-12-14 15:46:50 +0300

    point table icon and team names suceessfully styled

--- frontend/basketball-stats/components/PointsUserView.js
+++ frontend/basketball-stats/components/PointsUserView.js
@@ -164,128 +164,130 @@ const PointsUserView = ({ league }) => {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.initialView}>
-        {!selectedSeason && <h2>Select a competition to see score changes</h2>}
-        <div className={styles.selectors}>
-          <div className={styles.selectWrapper}>
-            <select 
-              value={selectedSeason} 
-              onChange={(e) => setSelectedSeason(e.target.value)}
-              className={selectedSeason ? styles.filled : ''}
-            >
-              <option value="">Season</option>
-              {seasons.map(season => (
-                <option key={season} value={season}>{season}-{season + 1}</option>
-              ))}
-            </select>
-            <span className={styles.selectLabel}>Season</span>
-          </div>
-          <div className={styles.customSelect}>
-            <div 
-              className={`${styles.selectedGame} ${selectedGame ? styles.filled : ''} ${!selectedSeason ? styles.disabled : ''}`}
-              onClick={() => selectedSeason && setShowGameDropdown(!showGameDropdown)}
-            >
-              {selectedGame ? (
-                <>
-                  <div className={styles.teamInfo}>
-                    <Image 
-                      src={currentGameTeams.team1?.logoUrl || '/teams_icons/default_team_icon.png'} 
-                      alt={`${currentGameTeams.team1?.fullName} logo`} 
-                      width={30} 
-                      height={30} 
-                      className={styles.teamLogo}
-                    />
-                    <span className={styles.teamName}>{currentGameTeams.team1?.fullName}</span>
-                  </div>
-                  <span className={styles.vsText}>vs</span>
-                  <div className={`${styles.teamInfo} ${styles.rightTeam}`}>
-                    <span className={styles.teamName}>{currentGameTeams.team2?.fullName}</span>
-                    <Image 
-                      src={currentGameTeams.team2?.logoUrl || '/teams_icons/default_team_icon.png'} 
-                      alt={`${currentGameTeams.team2?.fullName} logo`} 
-                      width={30} 
-                      height={30} 
-                      className={styles.teamLogo}
-                    />
-                  </div>
-                </>
-              ) : (
-                <span>Select a game</span>
-              )}
+    <div className={styles.containerWrapper}>
+      <div className={styles.container}>
+        <div className={styles.initialView}>
+          {!selectedGame && <h2>Select a competition to see score changes</h2>}
+          <div className={styles.selectors}>
+            <div className={styles.selectWrapper}>
+              <select 
+                value={selectedSeason} 
+                onChange={(e) => setSelectedSeason(e.target.value)}
+                className={selectedSeason ? styles.filled : ''}
+              >
+                <option value="">Season</option>
+                {seasons.map(season => (
+                  <option key={season} value={season}>{season}-{season + 1}</option>
+                ))}
+              </select>
+              <span className={styles.selectLabel}>Season</span>
             </div>
-            <span className={styles.selectLabel}>Game</span>
-            {showGameDropdown && (
-              <div className={styles.gameDropdown}>
-                {renderGameOptions()}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-      {selectedGame && (
-        <>
-          {loading && <p>Loading...</p>}
-          <div className={styles.scoreAttempts}>
-            {scoreAttempts.map((attempt, index) => {
-              const [team1, team2] = attempt.game.split('-');
-              const scoringTeam = attempt.points > 0 ? getTeamCode(attempt.season_team_id) : null;
-              const isTeam1Scoring = scoringTeam === team1;
-              return (
-                <div key={attempt.game_point_id} className={styles.attemptBlock}>
-                  <div className={styles.courtWrapper}>
-                    <Image src="/basketball-court.png" alt="Basketball Court" width={300} height={150} />
-                    <div 
-                      className={styles.shotMarker} 
-                      style={{ 
-                        left: `${(1500 + attempt.coord_y) / 7000  * 300}px`, 
-                        top: `${(950 + attempt.coord_x) / 1900 * 150}px` 
-                      }}
-                    />
-                    <div className={styles.minuteDisplay}>{attempt.minute}'</div>
-                  </div>
-                  <div className={styles.attemptInfo}>
+            <div className={styles.customSelect}>
+              <div 
+                className={`${styles.selectedGame} ${selectedGame ? styles.filled : ''} ${!selectedSeason ? styles.disabledGame : ''}`}
+                onClick={() => selectedSeason ? setShowGameDropdown(!showGameDropdown) : null}
+              >
+                {selectedGame ? (
+                  <>
                     <div className={styles.teamInfo}>
                       <Image 
-                        src={teamInfo[getTeamCode(attempt.season_team_id)]?.logoUrl || '/teams_icons/default_team_icon.png'} 
-                        alt="Team Logo" 
-                        width={60} 
-                        height={60} 
+                        src={currentGameTeams.team1?.logoUrl || '/teams_icons/default_team_icon.png'} 
+                        alt={`${currentGameTeams.team1?.fullName} logo`} 
+                        width={30} 
+                        height={30} 
                         className={styles.teamLogo}
                       />
-                      <span className={styles.playerName}>{formatPlayerName(attempt.player)}</span>
+                      <span className={styles.teamName}>{currentGameTeams.team1?.fullName}</span>
                     </div>
-                    <div className={styles.actionInfo}>
-                      <span>{attempt.action_of_play}</span>
-                      <span>
-                        <span className={attempt.points > 0 && isTeam1Scoring ? styles.scoringTeam : ''}>
-                          {attempt.points_a}
+                    <span className={styles.vsText}>vs</span>
+                    <div className={`${styles.teamInfo} ${styles.rightTeam}`}>
+                      <span className={styles.teamName}>{currentGameTeams.team2?.fullName}</span>
+                      <Image 
+                        src={currentGameTeams.team2?.logoUrl || '/teams_icons/default_team_icon.png'} 
+                        alt={`${currentGameTeams.team2?.fullName} logo`} 
+                        width={30} 
+                        height={30} 
+                        className={styles.teamLogo}
+                      />
+                    </div>
+                  </>
+                ) : (
+                  <span>Select a game</span>
+                )}
+              </div>
+              <span className={styles.selectLabel}>Game</span>
+              {showGameDropdown && (
+                <div className={styles.gameDropdown}>
+                  {renderGameOptions()}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        {selectedGame && (
+          <>
+            {loading && <p>Loading...</p>}
+            <div className={styles.scoreAttempts}>
+              {scoreAttempts.map((attempt, index) => {
+                const [team1, team2] = attempt.game.split('-');
+                const scoringTeam = attempt.points > 0 ? getTeamCode(attempt.season_team_id) : null;
+                const isTeam1Scoring = scoringTeam === team1;
+                return (
+                  <div key={attempt.game_point_id} className={styles.attemptBlock}>
+                    <div className={styles.courtWrapper}>
+                      <Image src="/basketball-court.png" alt="Basketball Court" width={300} height={150} />
+                      <div 
+                        className={styles.shotMarker} 
+                        style={{ 
+                          left: `${(1500 + attempt.coord_y) / 7000  * 300}px`, 
+                          top: `${(950 + attempt.coord_x) / 1900 * 150}px` 
+                        }}
+                      />
+                      <div className={styles.minuteDisplay}>{attempt.minute}'</div>
+                    </div>
+                    <div className={styles.attemptInfo}>
+                      <div className={styles.teamInfo}>
+                        <Image 
+                          src={teamInfo[getTeamCode(attempt.season_team_id)]?.logoUrl || '/teams_icons/default_team_icon.png'} 
+                          alt="Team Logo" 
+                          width={60} 
+                          height={60} 
+                          className={styles.teamLogo}
+                        />
+                        <span className={styles.playerName}>{formatPlayerName(attempt.player)}</span>
+                      </div>
+                      <div className={styles.actionInfo}>
+                        <span>{attempt.action_of_play}</span>
+                        <span>
+                          <span className={attempt.points > 0 && isTeam1Scoring ? styles.scoringTeam : ''}>
+                            {attempt.points_a}
+                          </span>
+                          {' - '}
+                          <span className={attempt.points > 0 && !isTeam1Scoring ? styles.scoringTeam : ''}>
+                            {attempt.points_b}
+                          </span>
                         </span>
-                        {' - '}
-                        <span className={attempt.points > 0 && !isTeam1Scoring ? styles.scoringTeam : ''}>
-                          {attempt.points_b}
-                        </span>
-                      </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-          </div>
-          <div className={styles.paginationControls}>
-            <button onClick={handlePrevious} disabled={offset === 0}>Previous</button>
-            <select 
-              value={rowsPerPage} 
-              onChange={(e) => setRowsPerPage(Number(e.target.value))}
-            >
-              <option value={25}>25 rows</option>
-              <option value={50}>50 rows</option>
-              <option value={100}>100 rows</option>
-            </select>
-            <button onClick={handleNext}>Next</button>
-          </div>
-        </>
-      )}
+                );
+              })}
+            </div>
+            <div className={styles.paginationControls}>
+              <button onClick={handlePrevious} disabled={offset === 0}>Previous</button>
+              <select 
+                value={rowsPerPage} 
+                onChange={(e) => setRowsPerPage(Number(e.target.value))}
+              >
+                <option value={25}>25 rows</option>
+                <option value={50}>50 rows</option>
+                <option value={100}>100 rows</option>
+              </select>
+              <button onClick={handleNext}>Next</button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
