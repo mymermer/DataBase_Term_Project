595e640819e5a5f333125839850190269a0e4531
itu-itis23-mahmoud21 <mahmoud21@itu.edu.tr>
2024-12-25 00:21:25 +0300

    Edited the header

--- frontend/basketball-stats/components/Header.js
+++ frontend/basketball-stats/components/Header.js
@@ -1,15 +1,41 @@
-import Link from 'next/link';
-import styles from '../styles/Header.module.css';
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
 
 export default function Header({ onLogoClick, alwaysVisible }) {
   return (
-    <header className={`${styles.header} ${alwaysVisible ? styles.alwaysVisible : ''}`}>
+    <header
+      className={`${styles.header} ${
+        alwaysVisible ? styles.alwaysVisible : ""
+      }`}
+    >
       <div className={styles.headerContent}>
         <Link href="/" onClick={onLogoClick} className={styles.homeLink}>
-          <h1>European Basketball Statistics</h1>
+          <div className={styles.logoWrapper}>
+            <Image
+              src="/footer-logo.png" // Path to your logo file
+              alt="Logo"
+              width={40} // Adjust width
+              height={40} // Adjust height
+              className={styles.logo}
+            />
+            <h1>European Basketball Statistics</h1>
+          </div>
         </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
 }
-
