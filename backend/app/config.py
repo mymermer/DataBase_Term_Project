caa387088aa7e3f9bb0acc31d7740e9e9db0ccd5
Muhammed Yusuf Mermer <myusufmermer@gmail.com>
2025-01-28 19:25:25 +0300

    added mysql and config files again

---
+++ backend/app/config.py
@@ -0,0 +1,7 @@
+class Config:
+    DB_HOST = "127.0.0.1"
+    DB_PORT = 3306
+    DB_USER = "your mysql user name"
+    DB_PASSWORD = "your mysql password"
+    DB_NAME = "basketball"
+    DEBUG = False
